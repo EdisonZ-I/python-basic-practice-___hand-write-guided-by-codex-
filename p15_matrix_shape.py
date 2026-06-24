295c984382080d5f5f9d67d754338367ff80615a
EdisonZ-I <zhouruipeng2006@outlook.com>
2026-06-24 23:32:41 +0800

    P1516 basic matrix

--- p15_matrix_shape.py
+++ p15_matrix_shape.py
@@ -1,1 +1,17 @@
+def matrix_shape(matrix):
+    if not matrix: return (0, 0)
+    else: return (len(matrix), len(matrix[0]))
+
+def is_rectangular(matrix):
+    if matrix:
+        num = len( matrix[0] )
+        for i in matrix:
+            if len( i ) != num: return False
+    return True
+
+if __name__ == "__main__":
+    assert matrix_shape([[1, 2], [3, 4]]) == (2, 2)
+    assert matrix_shape([]) == (0, 0)
+    assert is_rectangular([[1, 2], [3, 4]]) is True
+    assert is_rectangular([[1], [2, 3]]) is False
 
--- p16_transpose.py
+++ p16_transpose.py
@@ -1,1 +1,16 @@
+def transpose(matrix):
+    temp = []
+    result = []
+    if matrix:
+        for i in range(len(matrix[0])):
+            for j in matrix:
+                temp.append( j[i] )
+            result.append(temp)
+            temp=[]
+    return result
 
+if __name__ == "__main__":
+    m = [[1, 2, 3], [4, 5, 6]]
+    assert transpose(m) == [[1, 4], [2, 5], [3, 6]]
+    assert m == [[1, 2, 3], [4, 5, 6]]
+    assert transpose([]) == []
