ce1ff3721b5204ab95314bd2e0134445992cf83d
EdisonZ-I <zhouruipeng2006@outlook.com>
2026-06-25 00:07:43 +0800

    p17 18 basic matrix

--- p17_matrix_add.py
+++ p17_matrix_add.py
@@ -1,1 +1,16 @@
+def matrix_add(a, b):
+    '''test if same size'''
+    if len(a) != len(b): return None
+    for i in range( len(a) ):
+        if len(a[i]) != len(b[i]): return None
+    '''add'''
+    result = [[] for _ in range(len(a))]
+    for i in range( len(a) ):
+        for j in range( len(a[i])):
+            result[i].append(a[i][j] + b[i][j])
+    return result
 
+if __name__ =="__main__":
+    assert matrix_add([[1, 2], [3, 4]], [[10, 20], [30, 40]]) == [[11, 22], [33, 44]]
+    assert matrix_add([[1, 2]], [[1], [2]]) is None
+    assert matrix_add([], []) == []
--- p18_dot_product.py
+++ p18_dot_product.py
@@ -1,1 +1,11 @@
+def dot_product(a,b):
+    if len(a) != len(b): return None
+    result = 0
+    for i in range(len(a)):
+        result += a[i] * b[i]
+    return result
 
+if __name__ == "__main__":
+    assert dot_product([1, 2, 3], [4, 5, 6]) == 32
+    assert dot_product([1, 2], [3]) is None
+    assert dot_product([], []) == 0
